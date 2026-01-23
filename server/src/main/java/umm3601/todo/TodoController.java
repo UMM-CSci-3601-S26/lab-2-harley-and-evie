5d19501accc1b8c1eef352088a871a1865bdac4f
Harley Hannahs <hanna537@morris.umn.edu>
2026-01-23 11:45:31 -0600

    Simplified TodoController.java
    Deleted unnecessary code for issue #1 and added helpful comments to explain thought process. This will be merged into the Issue #1 branch if it is helpful.

--- server/src/main/java/umm3601/todo/TodoController.java
+++ server/src/main/java/umm3601/todo/TodoController.java
@@ -36,22 +36,14 @@ import umm3601.Controller;
 public class TodoController implements Controller {
 
   private static final String API_TODOS = "/api/todos";
-  private static final String API_TODO_BY_ID = "/api/todos/{id}";
-  static final String AGE_KEY = "age";
-  static final String COMPANY_KEY = "company";
-  static final String ROLE_KEY = "role";
-  static final String SORT_ORDER_KEY = "sortorder";
 
-  //private static final String API_TODO_BY_ID = "/api/todos/{id}";
+ // private static final String API_TODO_BY_ID = "/api/todos/{id}";
  // static final String OWNER_KEY = "owner";
  // static final String STATUS_KEY = "status";
  // static final String BODY_KEY = "body";
  // static final String CATEGORY_KEY = "category";
 
-
-  private static final int REASONABLE_AGE_LIMIT = 150;
-  private static final String ROLE_REGEX = "^(admin|editor|viewer)$";
-  public static final String EMAIL_REGEX = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+ // ^ needed for other issues -HH
 
   private final JacksonMongoCollection<Todo> todoCollection;
 
@@ -93,232 +85,25 @@ public class TodoController implements Controller {
   }
 
   /**
-   * Set the JSON body of the response to be a list of all the users returned from the database
+   * Set the JSON body of the response to be a list of all the todos returned from the database
    * that match any requested filters and ordering
    *
    * @param ctx a Javalin HTTP context
    */
   public void getTodos(Context ctx) {
-    Bson combinedFilter = constructFilter(ctx);
-    Bson sortingOrder = constructSortingOrder(ctx);
 
-    // All three of the find, sort, and into steps happen "in parallel" inside the
-    // database system. So MongoDB is going to find the users with the specified
-    // properties, return those sorted in the specified manner, and put the
-    // results into an initially empty ArrayList.
     ArrayList<Todo> matchingTodos = todoCollection
-      .find(combinedFilter)
-      .sort(sortingOrder)
+      .find()  //until we set up filtering, copilot helped with this -HH
       .into(new ArrayList<>());
 
-    // Set the JSON body of the response to be the list of users returned by the database.
+    // Set the JSON body of the response to be the list of todos returned by the database.
     // According to the Javalin documentation (https://javalin.io/documentation#context),
     // this calls result(jsonString), and also sets content type to json
     ctx.json(matchingTodos);
 
     // Explicitly set the context status to OK
     ctx.status(HttpStatus.OK);
-  }
-
-  /**
-   * Construct a Bson filter document to use in the `find` method based on the
-   * query parameters from the context.
-   *
-   * This checks for the presence of the `age`, `company`, and `role` query
-   * parameters and constructs a filter document that will match users with
-   * the specified values for those fields.
-   *
-   * @param ctx a Javalin HTTP context, which contains the query parameters
-   *    used to construct the filter
-   * @return a Bson filter document that can be used in the `find` method
-   *   to filter the database collection of users
-   */
-  private Bson constructFilter(Context ctx) {
-    List<Bson> filters = new ArrayList<>(); // start with an empty list of filters
-
-    if (ctx.queryParamMap().containsKey(AGE_KEY)) {
-      int targetAge = ctx.queryParamAsClass(AGE_KEY, Integer.class)
-        .check(it -> it > 0, "Todo's age must be greater than zero; you provided " + ctx.queryParam(AGE_KEY))
-        .check(it -> it < REASONABLE_AGE_LIMIT,
-          "Todo's age must be less than " + REASONABLE_AGE_LIMIT + "; you provided " + ctx.queryParam(AGE_KEY))
-        .get();
-      filters.add(eq(AGE_KEY, targetAge));
-    }
-    if (ctx.queryParamMap().containsKey(COMPANY_KEY)) {
-      Pattern pattern = Pattern.compile(Pattern.quote(ctx.queryParam(COMPANY_KEY)), Pattern.CASE_INSENSITIVE);
-      filters.add(regex(COMPANY_KEY, pattern));
-    }
-    if (ctx.queryParamMap().containsKey(ROLE_KEY)) {
-      String role = ctx.queryParamAsClass(ROLE_KEY, String.class)
-        .check(it -> it.matches(ROLE_REGEX), "Todo must have a legal user role")
-        .get();
-      filters.add(eq(ROLE_KEY, role));
-    }
-
-    // Combine the list of filters into a single filtering document.
-    Bson combinedFilter = filters.isEmpty() ? new Document() : and(filters);
-
-    return combinedFilter;
-  }
-
-  /**
-   * Construct a Bson sorting document to use in the `sort` method based on the
-   * query parameters from the context.
-   *
-   * This checks for the presence of the `sortby` and `sortorder` query
-   * parameters and constructs a sorting document that will sort users by
-   * the specified field in the specified order. If the `sortby` query
-   * parameter is not present, it defaults to "name". If the `sortorder`
-   * query parameter is not present, it defaults to "asc".
-   *
-   * @param ctx a Javalin HTTP context, which contains the query parameters
-   *   used to construct the sorting order
-   * @return a Bson sorting document that can be used in the `sort` method
-   *  to sort the database collection of users
-   */
-  private Bson constructSortingOrder(Context ctx) {
-    // Sort the results. Use the `sortby` query param (default "name")
-    // as the field to sort by, and the query param `sortorder` (default
-    // "asc") to specify the sort order.
-    String sortBy = Objects.requireNonNullElse(ctx.queryParam("sortby"), "name");
-    String sortOrder = Objects.requireNonNullElse(ctx.queryParam("sortorder"), "asc");
-    Bson sortingOrder = sortOrder.equals("desc") ?  Sorts.descending(sortBy) : Sorts.ascending(sortBy);
-    return sortingOrder;
-  }
-
-  /**
-   * Set the JSON body of the response to be a list of all the user names and IDs
-   * returned from the database, grouped by company
-   *
-   * This "returns" a list of user names and IDs, grouped by company in the JSON
-   * body of the response. The user names and IDs are stored in `TodoIdName` objects,
-   * and the company name, the number of users in that company, and the list of user
-   * names and IDs are stored in `TodoByCompany` objects.
-   *
-   * @param ctx a Javalin HTTP context that provides the query parameters
-   *   used to sort the results. We support either sorting by company name
-   *   (in either `asc` or `desc` order) or by the number of users in the
-   *   company (`count`, also in either `asc` or `desc` order).
-   */
-  public void getTodosGroupedByCompany(Context ctx) {
-    // We'll support sorting the results either by company name (in either `asc` or `desc` order)
-    // or by the number of users in the company (`count`, also in either `asc` or `desc` order).
-    String sortBy = Objects.requireNonNullElse(ctx.queryParam("sortBy"), "_id");
-    if (sortBy.equals("company")) {
-      sortBy = "_id";
-    }
-    String sortOrder = Objects.requireNonNullElse(ctx.queryParam("sortOrder"), "asc");
-    Bson sortingOrder = sortOrder.equals("desc") ?  Sorts.descending(sortBy) : Sorts.ascending(sortBy);
-
-    // The `TodoByCompany` class is a simple class that has fields for the company
-    // name, the number of users in that company, and a list of user names and IDs
-    // (using the `TodoIdName` class to store the user names and IDs).
-    // We're going to use the aggregation pipeline to group users by company, and
-    // then count the number of users in each company. We'll also collect the user
-    // names and IDs for each user in each company. We'll then convert the results
-    // of the aggregation pipeline to `TodoByCompany` objects.
-
-  }
-
-  /**
-   * Add a new user using information from the context
-   * (as long as the information gives "legal" values to Todo fields)
-   *
-   * @param ctx a Javalin HTTP context that provides the user info
-   *  in the JSON body of the request
-   */
-  public void addNewTodo(Context ctx) {
-    /*
-     * The follow chain of statements uses the Javalin validator system
-     * to verify that instance of `Todo` provided in this context is
-     * a "legal" user. It checks the following things (in order):
-     *    - The user has a value for the name (`usr.name != null`)
-     *    - The user name is not blank (`usr.name.length > 0`)
-     *    - The provided email is valid (matches EMAIL_REGEX)
-     *    - The provided age is > 0
-     *    - The provided age is < REASONABLE_AGE_LIMIT
-     *    - The provided role is valid (one of "admin", "editor", or "viewer")
-     *    - A non-blank company is provided
-     * If any of these checks fail, the Javalin system will throw a
-     * `BadRequestResponse` with an appropriate error message.
-     */
-    String body = ctx.body();
-    Todo newTodo = ctx.bodyValidator(Todo.class)
-      .check(usr -> usr.name != null && usr.name.length() > 0,
-        "Todo must have a non-empty user name; body was " + body)
-      .check(usr -> usr.email.matches(EMAIL_REGEX),
-        "Todo must have a legal email; body was " + body)
-      .check(usr -> usr.age > 0,
-        "Todo's age must be greater than zero; body was " + body)
-      .check(usr -> usr.age < REASONABLE_AGE_LIMIT,
-        "Todo's age must be less than " + REASONABLE_AGE_LIMIT + "; body was " + body)
-      .check(usr -> usr.role.matches(ROLE_REGEX),
-        "Todo must have a legal user role; body was " + body)
-      .check(usr -> usr.company != null && usr.company.length() > 0,
-        "Todo must have a non-empty company name; body was " + body)
-      .get();
-
-    // Generate a user avatar (you won't need this part for todos)
-    newTodo.avatar = generateAvatar(newTodo.email);
-
-    // Add the new user to the database
-    todoCollection.insertOne(newTodo);
-
-    // Set the JSON response to be the `_id` of the newly created user.
-    // This gives the client the opportunity to know the ID of the new user,
-    // which it can then use to perform further operations (e.g., a GET request
-    // to get and display the details of the new user).
-    ctx.json(Map.of("id", newTodo._id));
-    // 201 (`HttpStatus.CREATED`) is the HTTP code for when we successfully
-    // create a new resource (a user in this case).
-    // See, e.g., https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-    // for a description of the various response codes.
-    ctx.status(HttpStatus.CREATED);
-  }
-
-  /**
-   * Delete the user specified by the `id` parameter in the request.
-   *
-   * @param ctx a Javalin HTTP context
-   */
-  public void deleteTodo(Context ctx) {
-    String id = ctx.pathParam("id");
-    DeleteResult deleteResult = todoCollection.deleteOne(eq("_id", new ObjectId(id)));
-    // We should have deleted 1 or 0 users, depending on whether `id` is a valid user ID.
-    if (deleteResult.getDeletedCount() != 1) {
-      ctx.status(HttpStatus.NOT_FOUND);
-      throw new NotFoundResponse(
-        "Was unable to delete ID "
-          + id
-          + "; perhaps illegal ID or an ID for an item not in the system?");
-    }
-    ctx.status(HttpStatus.OK);
-  }
-
-  /**
-   * Utility function to generate an URI that points
-   * at a unique avatar image based on a user's email.
-   *
-   * This uses the service provided by gravatar.com; there
-   * are numerous other similar services that one could
-   * use if one wished.
-   *
-   * YOU DON'T NEED TO USE THIS FUNCTION FOR THE TODOS.
-   *
-   * @param email the email to generate an avatar for
-   * @return a URI pointing to an avatar image
-   */
-  String generateAvatar(String email) {
-    String avatar;
-    try {
-      // generate unique md5 code for identicon
-      avatar = "https://gravatar.com/avatar/" + md5(email) + "?d=identicon";
-    } catch (NoSuchAlgorithmException ignored) {
-      // set to mystery person
-      avatar = "https://gravatar.com/avatar/?d=mp";
-    }
-    return avatar;
-  }
+ }
 
   /**
    * Utility function to generate the md5 hash for a given string
@@ -337,23 +122,13 @@ public class TodoController implements Controller {
   }
 
   /**
-   * Sets up routes for the `user` collection endpoints.
-   * A TodoController instance handles the user endpoints,
+   * Sets up routes for the `todo` collection endpoints.
+   * A TodoController instance handles the todo endpoints,
    * and the addRoutes method adds the routes to this controller.
    *
    * These endpoints are:
-   *   - `GET /api/users/:id`
-   *       - Get the specified user
-   *   - `GET /api/users?age=NUMBER&company=STRING&name=STRING`
-   *      - List users, filtered using query parameters
-   *      - `age`, `company`, and `name` are optional query parameters
-   *   - `GET /api/usersByCompany`
-   *     - Get user names and IDs, possibly filtered, grouped by company
-   *   - `DELETE /api/users/:id`
-   *      - Delete the specified user
-   *   - `POST /api/users`
-   *      - Create a new user
-   *      - The user info is in the JSON body of the HTTP request
+   *   - `GET /api/todos/`
+   *       - Get a list of all todos
    *
    * GROUPS SHOULD CREATE THEIR OWN CONTROLLERS THAT IMPLEMENT THE
    * `Controller` INTERFACE FOR WHATEVER DATA THEY'RE WORKING WITH.
@@ -366,14 +141,7 @@ public class TodoController implements Controller {
    */
   @Override
   public void addRoutes(Javalin server) {
-    // Get the specified user
-    server.get(API_TODO_BY_ID, this::getTodos);
-
     // List users, filtered using query parameters
     server.get(API_TODOS, this::getTodos);
-
-    // Get the users, possibly filtered, grouped by company
-    server.get("/api/todosByCompany", this::getTodosGroupedByCompany);
-
   }
 }
